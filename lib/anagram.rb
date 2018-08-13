fb49530bd532b6fa61a0cd1da04e5881689cb13b
Sharon Mun <sharonymun@gmail.com>
2018-08-13 14:52:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :word
+  def initialize(word) #initialize sets attr. for an object when its created
+    @word = word
+  end
+  def match(array) #compare the attr. @word to the elements in the array
+    array.find_all do |string|
+      # string needed to be changed to an array of letters so .split was used. 
+      #Sort was used because order each element in the array
+     # To be able to compare arrays
+      string.split("").sort == @word.split("").sort
+    end
+  end
+end
